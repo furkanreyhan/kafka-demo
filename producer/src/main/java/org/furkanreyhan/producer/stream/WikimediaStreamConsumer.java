9700eea988669a6fc91e9364a4aa583e620f9b8c
furkanreyhan <furkanreyhan153@gmail.com>
2024-08-27 14:41:53 +0300

    initial commit

---
+++ producer/src/main/java/org/furkanreyhan/producer/stream/WikimediaStreamConsumer.java
@@ -0,0 +1,31 @@
+package org.furkanreyhan.producer.stream;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.furkanreyhan.producer.producer.WikimediaProducer;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+@Slf4j
+public class WikimediaStreamConsumer {
+
+    private final WebClient webClient;
+    private final WikimediaProducer producer;
+
+    public WikimediaStreamConsumer(WebClient.Builder webClientBuilder, WikimediaProducer producer) {
+        this.webClient = webClientBuilder
+                .baseUrl("https://stream.wikimedia.org/v2")
+                .build();
+        this.producer = producer;
+    }
+
+    public void consumeStreamAndPublish(){
+        webClient.get()
+                .uri("/stream/recentchange")
+                .retrieve()
+                .bodyToFlux(String.class)
+                .subscribe(producer::sendMessage);
+    }
+}
